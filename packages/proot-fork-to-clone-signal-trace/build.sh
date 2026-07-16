757f637aeaeb8d4250093aab3bdbeb06bbbb38d5
yourba <3312882215@qq.com>
2026-07-16 15:50:37 +0800

    Reset inherited PRoot tracee signal mask

--- packages/proot-fork-to-clone-signal-trace/build.sh
+++ packages/proot-fork-to-clone-signal-trace/build.sh
@@ -1,5 +1,5 @@
 PYSTUDIO_PROOT_FORK_TRACE=true
-PYSTUDIO_PROOT_EXTRA_PATCHES="pystudio-fork-trace.patch pystudio-fork-to-clone-seccomp.patch pystudio-signal-trace.patch"
+PYSTUDIO_PROOT_EXTRA_PATCHES="pystudio-fork-trace.patch pystudio-reset-tracee-signal-mask.patch pystudio-fork-to-clone-seccomp.patch pystudio-signal-trace.patch"
 _pystudio_package_dir="${TERMUX_PKG_BUILDER_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)}"
 . "$_pystudio_package_dir/../pystudio-proot-diagnostic-common/common.sh"
 pystudio_proot_variant "fork-to-clone-signal-trace" "fork.to.clone.signal.trace"
